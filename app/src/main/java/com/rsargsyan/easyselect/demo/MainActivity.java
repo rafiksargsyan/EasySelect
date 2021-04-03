4297872876fd050fc8a6b09a2064137ab958629e
Rafik Sargsyan <rafiksargsyan07@gmail.com>
2021-04-03 11:55:57 +0400

    Added some demo examples

--- app/src/main/java/com/rsargsyan/easyselect/demo/MainActivity.java
+++ app/src/main/java/com/rsargsyan/easyselect/demo/MainActivity.java
@@ -10,16 +10,32 @@ import com.rsargsyan.easyselect.EasySelectTextView.WordSpanningStrategy;
 
 public class MainActivity extends AppCompatActivity {
 
+    EasySelectTextView demoDefaultBehaviour;
+    EasySelectTextView demoCustomForegroundAndWordSelection;
+    EasySelectTextView demoNonDefaultInitialTextColor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.rsargsyan.easyselect.demo.R.layout.activity_main);
 
-        EasySelectTextView easySelectTextView = findViewById(R.id.easySelecDemoTextView);
-        easySelectTextView.setOnSelectionCompletedCallback(selectedString ->
-                Toast.makeText(MainActivity.this,
-                        selectedString, Toast.LENGTH_SHORT).show());
-        easySelectTextView.setSpanningStrategy(WordSpanningStrategy.getInstance());
-        easySelectTextView.setText("Let's have some coffee");
+        demoDefaultBehaviour = findViewById(R.id.demoDefaultBehaviour);
+        demoDefaultBehaviour.setOnSelectionCompletedCallback(selectedString -> {
+            Toast.makeText(MainActivity.this, selectedString, Toast.LENGTH_SHORT).show();
+        });
+
+        demoCustomForegroundAndWordSelection =
+                findViewById(R.id.demoCustomForegroundAndWordSelection);
+        demoCustomForegroundAndWordSelection.setSpanningStrategy(
+                EasySelectTextView.WordSpanningStrategy.getInstance());
+        demoCustomForegroundAndWordSelection.setSelectionTextHighlightColor(0);
+        demoCustomForegroundAndWordSelection.setOnSelectionCompletedCallback(selectedString -> {
+            Toast.makeText(MainActivity.this, selectedString, Toast.LENGTH_SHORT).show();
+        });
+
+        demoNonDefaultInitialTextColor = findViewById(R.id.demoNonDefaultInitialTextColor);
+        demoNonDefaultInitialTextColor.setOnSelectionCompletedCallback(selectedString -> {
+            Toast.makeText(MainActivity.this, selectedString, Toast.LENGTH_SHORT).show();
+        });
     }
 }
